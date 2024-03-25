4d580ff0bbf365ffce0bdaa39e2c57af384e2e69
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-25 14:08:27 -0500

    Adjust root path

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser } = require('.authController./controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const tasksController = require('.tasksController./controllers/tasksController');
+const tasksController = require('../controllers/tasksController');
 const router = express.Router();
 
 router.post('/', tasksController.createTask);
-
 router.get('/', tasksController.getAllTasks);
-
 router.get('/:id', tasksController.getTaskById);
-
 router.patch('/:id', tasksController.updateTask);
-
 router.delete('/:id', tasksController.deleteTask);
 
 module.exports = router;
