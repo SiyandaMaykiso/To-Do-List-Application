057b426358621299867ecb7c20d53abd18de812c
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-28 11:54:52 -0500

    Server.js optimized for heroku

--- server.js
+++ server.js
@@ -4,25 +4,29 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Enhanced CORS options to handle both development and production environments
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: process.env.NODE_ENV === 'production' ? 'https://to-do-list-application-sm-79db330bd202.herokuapp.com' : 'http://localhost:3000',
     credentials: true,
 };
 
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
-app.use(cors(corsOptions));
-app.use(express.json());
+app.use(cors(corsOptions)); // Setup CORS with the specified options
+app.use(express.json()); // Middleware to parse JSON
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
+// All remaining requests return the React app, so it can handle routing.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-
+// Start the server on the specified port
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
