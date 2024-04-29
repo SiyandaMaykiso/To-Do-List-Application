1ea1f8dc7b06cffc02ec566124aa6d0f5bf2aed4
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-28 23:23:58 -0500

    Add Heroku postgres

--- db.js
+++ db.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+let poolConfig = {
+  connectionString: process.env.DATABASE_URL
+};
+
+if (process.env.NODE_ENV === 'production') {
+  poolConfig.ssl = {
+    rejectUnauthorized: false // Necessary for Heroku's self-signed certificates
+  };
+} else {
+  // Additional configuration for local development if needed
+  poolConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+  };
+}
+
+const pool = new Pool(poolConfig);
 
 module.exports = pool;
